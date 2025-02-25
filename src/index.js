85e360c61c651dbaad4ae405f579480b259cdae1
Patmarie Adegbuyi <PATMARIE.ADEGBUYI.2022@MUMAIL.IE>
2025-02-25 13:20:21 +0000

    changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter basename="/final-year-project-app">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
